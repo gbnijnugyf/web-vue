f72f2bc5fba2c35b4bb62fa6766f277c7b9eb417
gbnijnugyf <1612517397@qq.com>
2024-04-22 15:26:37 +0800

    fix: fix forget View

--- src/views/ForgetView.vue
+++ src/views/ForgetView.vue
@@ -3,124 +3,236 @@ import { reactive, ref } from "vue";
 import router from "@/router";
 import type { FormInstance, FormRules } from "element-plus";
 
-const ruleFormRef = ref<FormInstance>();
+const ruleFormRefLast = ref<FormInstance>();
+const ruleFormRefNew = ref<FormInstance>();
+
 
 const checkUsername = (rule: any, value: any, callback: any) => {
-    if (!value) {
-        return callback(new Error("请输入用户名"));
-    }
+  if (!value) {
+    return callback(new Error("请输入用户名"));
+  }
+  callback();
 };
-
 
 const validatePass = (rule: any, value: any, callback: any) => {
-    if (value === "") {
-        callback(new Error("请输入密码"));
-    } else {
-        if (ruleForm.checkPass !== "") {
-            if (!ruleFormRef.value) return;
-            ruleFormRef.value.validateField("checkPass", () => null);
-        }
-        callback();
-    }
+  if (value === "") {
+    callback(new Error("请输入密码"));
+  } else {
+    callback();
+  }
 };
+
 const validatePass2 = (rule: any, value: any, callback: any) => {
-    if (value === "") {
-        callback(new Error("请再次输入密码"));
-    } else if (value !== ruleForm.pass) {
-        callback(new Error("两次输入密码不一致"));
-    } else {
-        callback();
+  if (value === "") {
+    callback(new Error("请再次输入密码"));
+  } else if (value !== ruleFormNew.newPass) {
+    callback(new Error("两次输入密码不一致"));
+  } else {
+    callback();
+  }
+};
+
+const ruleFormLast = reactive({
+  username: "",
+  pass: "",
+});
+const ruleFormNew = reactive({
+  newPass: "",
+  checkPass: "",
+});
+
+const rulesLast = reactive<FormRules<typeof ruleFormLast>>({
+  pass: [{ validator: validatePass, trigger: "blur" }],
+  username: [{ validator: checkUsername, trigger: "blur" }],
+});
+const rulesNew = reactive<FormRules<typeof ruleFormNew>>({
+  newPass: [{ validator: validatePass, trigger: "blur" }],
+  checkPass: [{ validator: validatePass2, trigger: "blur" }],
+});
+
+const submitForm = () => {
+  if (!ruleFormRefLast.value) return;
+  ruleFormRefLast.value.validate((valid) => {
+    if (!valid) {
+      console.log("error submit!");
+      return false;
     }
-};
-
-const ruleForm = reactive({
-    username: "",
-    pass: "",
-    checkPass: "",
-});
-
-const rules = reactive<FormRules<typeof ruleForm>>({
-    pass: [{ validator: validatePass, trigger: "blur" }],
-    checkPass: [{ validator: validatePass2, trigger: "blur" }],
-    username: [{ validator: checkUsername, trigger: "blur" }],
-});
-
-const submitForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return;
-    formEl.validate((valid) => {
-        if (valid) {
-            console.log("submit!");
-        } else {
-            console.log("error submit!");
-            return false;
-        }
+    if (!ruleFormRefNew.value) return;
+    ruleFormRefNew.value.validate((valid) => {
+      if (valid) {
+        console.log("submit!");
+      } else {
+        console.log("error submit!");
+        return false;
+      }
     });
-};
-
-const resetForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return;
-    formEl.resetFields();
+  });
 };
 
 const returnLogin = () => {
-    router.push("/");
+  router.push("/");
 };
 </script>
 
 <template>
-    <div class="forget-container">
-        <el-row>
-            <el-col :span="24" class="forget-header">修改密码</el-col>
-            <el-col class="forget-pannel">
-                <el-form ref="ruleFormRef" style="max-width: 600px" :model="ruleForm" status-icon :rules="rules"
-                    label-width="auto" class="demo-ruleForm">
-                    <el-form-item prop="username">
-                        <el-input v-model.number="ruleForm.username" placeholder="用户名" />
-                    </el-form-item>
-                    <el-form-item prop="pass">
-                        <el-input v-model="ruleForm.pass" type="password" autocomplete="off" placeholder="新密码" />
-                    </el-form-item>
-                    <el-form-item prop="checkPass">
-                        <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" placeholder="确认密码" />
-                    </el-form-item>
+  <div class="register-container">
+    <el-row>
+      <el-col :span="24" class="register-header">修改密码</el-col>
+    </el-row>
 
-                    <el-form-item>
-                        <el-button type="primary" @click="submitForm(ruleFormRef)">
-                            确认
-                        </el-button>
-                    </el-form-item>
-                </el-form>
-            </el-col>
-            <el-col><el-button @click="returnLogin">返回登录界面</el-button></el-col>
-        </el-row>
-    </div>
+    <el-row class="register-pannel">
+      <el-col :xs="24" :sm="12">
+        <el-form
+          ref="ruleFormRefLast"
+          style="max-width: 600px"
+          :model="ruleFormLast"
+          status-icon
+          :rules="rulesLast"
+          label-width="auto"
+          class="register-form"
+        >
+          <el-form-item prop="username">
+            <el-input v-model="ruleFormLast.username" placeholder="用户名" />
+          </el-form-item>
+          <el-form-item prop="pass">
+            <el-input
+              v-model="ruleFormLast.pass"
+              type="password"
+              autocomplete="off"
+              placeholder="密码"
+            />
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-col :xs="24" :sm="12">
+        <el-form
+          ref="ruleFormRefNew"
+          style="max-width: 600px"
+          :model="ruleFormNew"
+          status-icon
+          :rules="rulesNew"
+          label-width="auto"
+          class="register-form"
+        >
+          <el-form-item prop="newPass">
+            <el-input
+              v-model="ruleFormNew.newPass"
+              placeholder="新密码"
+              type="password"
+            />
+          </el-form-item>
+          <el-form-item prop="checkPass">
+            <el-input
+              v-model="ruleFormNew.checkPass"
+              type="password"
+              autocomplete="off"
+              placeholder="密码"
+            />
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12">
+        <el-button
+          class="other"
+          id="register"
+          @click="submitForm()"
+          >修改密码</el-button
+        >
+      </el-col>
+      <el-col :span="12">
+        <el-button class="other" id="forget" @click="returnLogin"
+          >返回登录界面</el-button
+        >
+      </el-col>
+    </el-row>
+    <!-- <el-row
+      ><el-col
+        ><el-button @click="returnLogin">返回登录界面</el-button></el-col
+      ></el-row
+    > -->
+  </div>
 </template>
 
 <style lang="scss">
-.forget-container {
-    margin: 0 auto;
-    width: 30%;
-    min-width: 300px;
-    .forget-header {
-        text-align: center;
-        font-size: 24px;
-    }
+.register-container {
+  margin: 0 auto;
+  margin-top: 5%;
 
-    .forget-pannel {
-        display: flex;
-        justify-content: center;
+  width: 80%;
+  min-width: 300px;
 
-        .el-button {
-            width: 100%;
-            margin: 0 auto;
-            background-color: rgb(122,183,113);
-            border: none;
-        }
+  .register-header {
+    text-align: center;
+    font-size: 24px;
+  }
+
+  .register-pannel {
+    display: flex;
+    justify-content: center;
+    .register-form {
+      margin-top: 4%;
+      margin-left: 10%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 80%;
     }
-    .el-button{
-        background-color: rgb(102,102,102);
-        color: white;
-        width: 100%
+    .el-button {
+      width: 100%;
+      margin: 0 auto;
+      margin-top: 5%;
+      background-color: rgb(122, 183, 113);
+      border: none;
     }
+  }
+  .register-pannel:before {
+    content: "";
+    position: absolute;
+    top: 10%;
+    bottom: 10%;
+    left: 50%;
+    border-left: 2px solid rgb(233, 233, 233);
+    // border-bottom: 2px solid red;
+  }
+  @media screen and (max-width: 768px) {
+    .register-pannel:before {
+      top: 60%;
+      left: 10%;
+      right: 10%;
+      bottom: auto;
+      border-left: none;
+      border-top: 2px solid rgb(233, 233, 233);
+    }
+  }
+  .el-button {
+    background-color: rgb(102, 102, 102);
+    color: white;
+    width: 100%;
+  }
+}
+.avatar-uploader > .el-upload {
+  width: 50%;
+  margin-left: 25%;
+  //   margin-top: 6%;
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .avatar {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 120px;
+  height: 120px;
+  line-height: 120px;
+  text-align: center;
 }
 </style>
